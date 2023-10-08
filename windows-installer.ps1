eb76b25beb06b23d8dbb190146a385086006076d
Orbit Turner <orbitturner@gmail.com>
2023-10-08 14:05:06 +0000

    feat: Enhance Windows installer script
    - Added checks for existing installation in Program Files and environment variables
    - Prompt user for reinstallation if program is already installed
    - Improved handling of temporary directory, cleaning up previous clones
    - Added emoji and detailed log messages for each installation step

--- windows-installer.ps1
+++ windows-installer.ps1
@@ -6,10 +6,41 @@ if (-not $adminRights) {
     return Write-Host "❌ Please Bro RUN this script as an SUDO. Right-click the PowerShell icon and select 'Run as Administrator.' 😫😫 Are you Mad ?!"
 }
 
+# Détermine le répertoire où stocker les applications
+$app_directory = "C:\Program Files\OrbitFastDataLoader"
+
 Write-Host "💻 === Windows Installation === 💻"
+
+if (Test-Path $app_directory) {
+    $reinstall = $null
+    while ($reinstall -ne 'Y' -and $reinstall -ne 'N') {
+        $reinstall = Read-Host "Program is already installed at $app_directory. Do you want to reinstall? (Y/N)"
+    }
+
+    if ($reinstall -eq 'Y') {
+        # Supprimer le programme existant et son entrée dans l'environnement
+        Write-Host "🗑 Uninstalling existing program..."
+        Remove-Item -Recurse -Force $app_directory
+
+        # Supprimer l'entrée dans l'environnement
+        $envPath = [System.Environment]::GetEnvironmentVariable('Path', [System.EnvironmentVariableTarget]::Machine)
+        $newPath = $envPath -replace [regex]::Escape("$app_directory;"), ''
+        [System.Environment]::SetEnvironmentVariable('Path', $newPath, [System.EnvironmentVariableTarget]::Machine)
+    } else {
+        return Write-Host "🚫 Installation aborted by user."
+    }
+}
+
 
 # Clone le repo dans le répertoire temporaire
 $temp_directory = [System.IO.Path]::Combine($env:TEMP, 'orbit-fast-data-loader')
+
+# Supprime le répertoire temporaire s'il existe déjà
+if (Test-Path $temp_directory) {
+    Write-Host "🗑 Removing existing directory: $temp_directory"
+    Remove-Item $temp_directory -Recurse -Force
+}
+
 Write-Host "🔄 Cloning the repository to $temp_directory..."
 git clone https://github.com/orbitturner/orbit-fast-data-loader $temp_directory
 
@@ -23,9 +54,6 @@ npm install
 # Construit le projet
 Write-Host "🏗 Building the project..."
 npm run build
-
-# Détermine le répertoire où stocker les applications
-$app_directory = "C:\Program Files\OrbitFastDataLoader"
 
 # Crée le répertoire s'il n'existe pas
 Write-Host "📁 Creating directory: $app_directory"
